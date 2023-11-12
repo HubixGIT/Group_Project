c4bc34ee40775ac5f48a4b0f145368fcb4505a8c
Maciej Bieszczad <60392632+MacBie2k@users.noreply.github.com>
2023-11-12 22:03:41 +0100

    Add User entity; create user endpoint added (#2)

---
+++ server/Web.Api/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.Api.Entities;
+
+public class User
+{
+   public Guid Id { get; set; }
+   public string Email { get; set; }
+   public string Password { get; set; }
+   public string FullName { get; set; }
+   public DateTime CreatedOnUtc { get; set; }
+}
